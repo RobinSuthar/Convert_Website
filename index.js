3f7513148995233377c6d9174c7f14063fd0dbba
Sirisak (Mark) <139118914+MarkH4236@users.noreply.github.com>
2024-10-06 10:30:07 -0600

    Update index.js
    add miles to kilo function

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+// Pounds to Kilograms
 document.getElementById("convertButtonKP").addEventListener("click", () => {
     const poundsInput = document.getElementById("userInputedPounds");
     const kilogramsInput = document.getElementById("userInputedKilograms");
@@ -12,5 +13,22 @@ document.getElementById("convertButtonKP").addEventListener("click", () => {
     } else {
         alert("Please enter a value to convert.");
     }
+});
+
+// Miles to Kilometers
+document.getElementById("convertButtonMK").addEventListener("click", () => {
+  const milesInput = document.getElementById("userInputedMiles");
+  const kilometersInput = document.getElementById("userInputedKilometers");
+  if (milesInput.value) {
+    const miles = parseFloat(milesInput.value);
+    const kilometers = (miles * 1.60934).toFixed(2);
+    kilometersInput.value = kilometers;
+  } else if (kilometersInput.value) {
+    const kilometers = parseFloat(kilometersInput.value);
+    const miles = (kilometers / 1.60934).toFixed(2);
+    milesInput.value = miles;
+  } else {
+    alert("Please enter a value to convert.");
+  }
 });
 
